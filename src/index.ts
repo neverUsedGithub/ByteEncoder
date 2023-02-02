029e17364feef8091f7f1a0977953957f086f167
neverUsedGithub <hulyevagyok420@gmail.com>
2023-02-02 21:11:50 +0100

    switch to tsup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-class ByteDecoder {
+type AnyJSON = { [key: string]: any };
+type DecodedType = number | bigint | string | AnyJSON;
+
+export class ByteDecoder {
+  buffer: DataView;
+  schema: AnyJSON[];
+  offset: number;
+
   constructor() {
-    this.buffer = null;
     this.schema = [];
     this.offset = 0;
   }
@@ -16,9 +22,9 @@ class ByteDecoder {
   static u64() { return { type: "u", size: 64 } }
   static f32() { return { type: "f", size: 32 } }
   static f64() { return { type: "f", size: 64 } }
-  static array(type) { return { type: "array", itemType: type } }
-  static map(keyType, valueType) { return { type: "map", keyType, valueType } }
-  static struct(entries) { return { type: "struct", entries } }
+  static array(type: AnyJSON) { return { type: "array", itemType: type } }
+  static map(keyType: AnyJSON, valueType: AnyJSON) { return { type: "map", keyType, valueType } }
+  static struct(entries: AnyJSON) { return { type: "struct", entries } }
   // Aliases
   static int() { return { type: "i", size: 32 } }
   static uint() { return { type: "u", size: 32 } }
@@ -28,7 +34,7 @@ class ByteDecoder {
     return new ByteDecoder().add(...items);
   }
 
-  __parseInt(size) {
+  __parseInt(size: number) {
     if (size === 8) return this.buffer.getInt8(this.offset ++);
     else if (size === 16) { this.offset += 2; return this.buffer.getInt16(this.offset - 2); }
     else if (size === 32) { this.offset += 4; return this.buffer.getInt32(this.offset - 4); }
@@ -37,7 +43,7 @@ class ByteDecoder {
     throw new Error(`Invalid int size ${size}.`)
   }
 
-  __parseUint(size) {
+  __parseUint(size: number) {
     if (size === 8) return this.buffer.getUint8(this.offset ++);
     else if (size === 16) { this.offset += 2; return this.buffer.getUint16(this.offset - 2); }
     else if (size === 32) { this.offset += 4; return this.buffer.getUint32(this.offset - 4); }
@@ -46,7 +52,7 @@ class ByteDecoder {
     throw new Error(`Invalid uint size ${size}.`)
   }
 
-  __parseFloat(size) {
+  __parseFloat(size: number) {
     if (size === 32) { this.offset += 4; return this.buffer.getFloat32(this.offset - 4); }
     if (size === 64) { this.offset += 8; return this.buffer.getFloat64(this.offset - 8); }
     
@@ -63,9 +69,9 @@ class ByteDecoder {
     return content;
   }
 
-  __parseArray(type) {
+  __parseArray(type: AnyJSON) {
     const arraySize = this.__parseUint(8);
-    const arr = [];
+    const arr: DecodedType[] = [];
 
     for (let i = 0; i < arraySize; i++)
       arr.push(this.__parse(type));
@@ -73,12 +79,12 @@ class ByteDecoder {
     return arr;
   }
 
-  __parseMap(keyType, valueType) {
+  __parseMap(keyType: AnyJSON, valueType: AnyJSON) {
     const mapSize = this.__parseUint(8);
-    const value = {};
+    const value: AnyJSON = {};
 
     for (let i = 0; i < mapSize; i++) {
-      value[this.__parse(keyType)] = this.__parse(valueType);
+      value[this.__parse(keyType) as any] = this.__parse(valueType);
     }
 
     return value;
@@ -100,7 +106,7 @@ class ByteDecoder {
     return value;
   }
 
-  __parse(part) {
+  __parse(part: AnyJSON): DecodedType {
     if (part.type === "i")
       return this.__parseInt(part.size);
     
@@ -121,13 +127,15 @@ class ByteDecoder {
 
     if (part.type === "struct")
       return this.__parseStruct(part.entries);
+
+    throw new Error("Invalid type.")
   }
 
   add(...types) { this.schema = this.schema.concat(types); return this; }
   
   decode(data) {
     this.buffer = new DataView(data);
-    const parsed = [];
+    const parsed: DecodedType[] = [];
 
     for (const part of this.schema) {
       parsed.push(this.__parse(part))
@@ -137,39 +145,41 @@ class ByteDecoder {
   }
 }
 
-class ByteEncoder {
+export class ByteEncoder {
+  buffer: DataView;
+  offset: number;
+
   constructor() {
     this.buffer = new DataView(new ArrayBuffer(999999));
     this.offset = 0;
-    this.toenc = [];
   }
 
-  __addInt(size, value) {
+  __addInt(size: number, value: number) {
     if (size === 8)  this.buffer.setInt8(this.offset ++, value);
     if (size === 16) { this.buffer.setInt16(this.offset, value); this.offset += 2; };
     if (size === 32) { this.buffer.setInt32(this.offset, value); this.offset += 4; };
-    if (size === 64) { this.buffer.setBigInt64(this.offset, value); this.offset += 8; };
+    if (size === 64) { this.buffer.setBigInt64(this.offset, value as unknown as bigint); this.offset += 8; };
   }
 
-  __addUint(size, value) {
+  __addUint(size: number, value: number) {
     if (size === 8)  this.buffer.setUint8(this.offset ++, value);
     if (size === 16) { this.buffer.setUint16(this.offset, value); this.offset += 2; };
     if (size === 32) { this.buffer.setUint32(this.offset, value); this.offset += 4; };
-    if (size === 64) { this.buffer.setBigUint64(this.offset, value); this.offset += 8; };
+    if (size === 64) { this.buffer.setBigUint64(this.offset, value as unknown as bigint); this.offset += 8; };
   }
 
-  __addFloat(size, value) {
+  __addFloat(size: number, value: number) {
     if (size === 32) { this.buffer.setFloat32(this.offset, value); this.offset += 4; }
     if (size === 64) { this.buffer.setFloat64(this.offset, value); this.offset += 8; }
   }
 
-  __addString(value) {
+  __addString(value: string) {
     for (const char of value)
       this.buffer.setUint8(this.offset ++, char.charCodeAt(0));
     this.buffer.setUint8(this.offset ++, 0);
   }
 
-  __add(item) {
+  __add(item: AnyJSON) {
     if (item.type === "string")
       return this.__addString(item.value);
 
@@ -205,36 +215,36 @@ class ByteEncoder {
 
       for (const [key, val] of Object.entries(item.entries)) {
         this.__addString(key);
-        this.__add(val);
+        this.__add(val as AnyJSON);
       }
       return;
     }
   }
 
-  add(...items) {
+  add(...items: AnyJSON[]) {
     for (const item of items)
       this.__add(item)
     return this;
   }
 
-  static string(str) { return { type: "string", value: str }; }
-  static i8(val)  { return { type: "i", size: 8, value: val }; }
-  static i16(val) { return { type: "i", size: 16, value: val }; }
-  static i32(val) { return { type: "i", size: 32, value: val }; }
-  static i64(val) { return { type: "i", size: 64, value: val }; }
-  static u8(val)  { return { type: "u", size: 8, value: val }; }
-  static u16(val) { return { type: "u", size: 16, value: val }; }
-  static u32(val) { return { type: "u", size: 32, value: val }; }
-  static u64(val) { return { type: "u", size: 64, value: val }; }
-  static f32(val) { return { type: "f", size: 32, value: val } }
-  static f64(val) { return { type: "f", size: 64, value: val } }
-  static array(...values) { return { type: "array", values } } 
-  static map(...entries) { return { type: "map", entries } }
-  static struct(entries) { return { type: "struct", entries } }
+  static string(str: string) { return { type: "string", value: str }; }
+  static i8(val: number)  { return { type: "i", size: 8, value: val }; }
+  static i16(val: number) { return { type: "i", size: 16, value: val }; }
+  static i32(val: number) { return { type: "i", size: 32, value: val }; }
+  static i64(val: bigint) { return { type: "i", size: 64, value: val }; }
+  static u8(val: number)  { return { type: "u", size: 8, value: val }; }
+  static u16(val: number) { return { type: "u", size: 16, value: val }; }
+  static u32(val: number) { return { type: "u", size: 32, value: val }; }
+  static u64(val: bigint) { return { type: "u", size: 64, value: val }; }
+  static f32(val: number) { return { type: "f", size: 32, value: val } }
+  static f64(val: bigint) { return { type: "f", size: 64, value: val } }
+  static array(...values: AnyJSON[]) { return { type: "array", values } } 
+  static map(...entries: AnyJSON[]) { return { type: "map", entries } }
+  static struct(entries: AnyJSON) { return { type: "struct", entries } }
   // Aliases
-  static int(val) { return { type: "i", size: 32, value: val } }
-  static uint(val) { return { type: "u", size: 32, value: val } }
-  static float(val) { return { type: "f", size: 32, value: val } }
+  static int(val: number) { return { type: "i", size: 32, value: val } }
+  static uint(val: number) { return { type: "u", size: 32, value: val } }
+  static float(val: number) { return { type: "f", size: 32, value: val } }
   
   static encode(...items) {
     return new ByteEncoder().add(...items)._encode();
@@ -245,9 +255,5 @@ class ByteEncoder {
   }
 }
 
-module.exports = {
-  bd: ByteDecoder,
-  be: ByteEncoder,
-  ByteDecoder,
-  ByteEncoder
-}
+export const bd = ByteDecoder;
+export const be = ByteEncoder;
